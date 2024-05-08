bc8bb96dea433b3ce4cb5622a59e385e14edbdcd
HaiDang <lthdang@ninepoints.vn>
2024-05-08 11:39:16 +0700

    build Ui header  #1

--- src/components/Layout/DefaultLayout/Sildebar/index.js
+++ src/components/Layout/DefaultLayout/Sildebar/index.js
@@ -1,5 +1,12 @@
+import styles from './Sildebar.module.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
 function Silebar() {
-  return <h2>Silebar</h2>;
+  return (
+    <aside className={cx('wrapper')}>
+      <h2>Sildebar</h2>
+    </aside>
+  );
 }
 
 export default Silebar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,12 +1,15 @@
 import Header from '~/components/Layout/components/Header';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 import Sildebar from './Sildebar';
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div className="container">
+      <div className={cx('container')}>
         <Sildebar />
-        <div className="content">{children}</div>
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
