8978c4996097ae8bb81738e4dc1b4e68c829047f
HaiDang <lthdang@ninepoints.vn>
2024-05-14 08:11:46 +0700

    add account

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 const images = {
   logo: require('~/assets/images/logo.svg').default,
+  dragon: require('~/assets/images/dragon.svg').default,
 };
 export default images;
--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,17 +2,13 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
-
+import images from '~/assets/images';
 const cx = classNames.bind(styles);
 
 function AccountItem() {
   return (
     <div className={cx('wrapper')}>
-      <img
-        className={cx('avatar')}
-        src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/a30e6a7901525df077275b7886554a0d.jpeg?lk3s=a5d48078&x-expires=1715414400&x-signature=LXfImB6O1JCdEHUTdQqrvCJmP7Q%3D"
-        alt="Hoaa"
-      />
+      <img className={cx('avatar')} src={images.dragon} alt="Hoaa" />
       <div className={cx('info')}>
         <h4 className={cx('name')}>
           <span>Nguyen Van A</span>
